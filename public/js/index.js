046b97f75be0c5ba9fa893dfabcd1473eb38c04a
Soo <soojlee0106@naver.com>
2023-05-28 21:38:07 +0900

    add server polling

--- public/js/index.js
+++ public/js/index.js
@@ -19,25 +19,29 @@ form.addEventListener('submit', function (event) {
 
     body.classList.add('show-loading');
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function () {
-        body.classList.remove('show-loading');
+    function pollServer() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function () {
+            body.classList.remove('show-loading');
 
-        if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            document.querySelector('#results').innerHTML =
-                "<p>" + response['languages'] + "</p>" +
-                "<p>" + response['contributions'] + "</p>" +
-                "<p>" + response['years_active'] + "</p>" +
-                "<p> See Profile: <a href='" + repo_url + "'>" + repo_url + "</a></p>";
-        } else {
-            var error = JSON.parse(xhr.responseText).error;
-            document.querySelector('#results').innerHTML = "<p>Oops, " + error + "</p>";
-        }
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                document.querySelector('#results').innerHTML =
+                    "<p>" + response['languages'] + "</p>" +
+                    "<p>" + response['contributions'] + "</p>" +
+                    "<p>" + response['years_active'] + "</p>" +
+                    "<p> See Profile: <a href='" + repo_url + "'>" + repo_url + "</a></p>";
+            } else {
+                var error = JSON.parse(xhr.responseText).error;
+                document.querySelector('#results').innerHTML = "<p>Oops, " + error + "</p>";
+                setTimeout(pollServer, 1000);
+            }
+        };
+        xhr.send(JSON.stringify(data));
     };
 
-    xhr.send(JSON.stringify(data));
-
+    pollServer();
+    
 });
