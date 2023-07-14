7d7ad8e532becaf91ed2f77f517037ec58dc0835
MontyCoder0701 <soojlee0106@naver.com>
2023-07-14 17:16:56 +0900

    Change date format

--- public/js/year_calc.js
+++ public/js/year_calc.js
@@ -2,22 +2,18 @@ var year_form = document.getElementById('year-form');
 var add_year = document.getElementById('add-year');
 
 var yearFieldsContainer = document.getElementById('year-fields-container');
-var fieldIndex = 1; // Used to generate unique IDs for the new fields
+var fieldIndex = 1; 
 
 add_year.addEventListener('click', function (event) {
     event.preventDefault();
 
-    // Create a new set of fields
     var newFields = document.createElement('div');
     newFields.innerHTML = `
-        <input type="date" class="start-year" id="start-year-${fieldIndex}" name="start-year-${fieldIndex}">
-        <input type="date" class="end-year" id="end-year-${fieldIndex}" name="end-year-${fieldIndex}">
+        <input type="text" class="start-year" id="start-year-${fieldIndex}" name="start-year-${fieldIndex}"  placeholder="Start">
+        <input type="text" class="end-year" id="end-year-${fieldIndex}" name="end-year-${fieldIndex}" placeholder="End">
     `;
 
-    // Append the new fields to the container
     yearFieldsContainer.appendChild(newFields);
-
-    // Increment the field index
     fieldIndex++;
 });
 
@@ -31,11 +27,26 @@ year_form.addEventListener('submit', function (event) {
     var differences = [];
 
     for (var i = 0; i < startYearInputs.length; i++) {
-        var startYear = new Date(startYearInputs[i].value);
-        var endYear = new Date(endYearInputs[i].value);
+        var startYear = parseDate(startYearInputs[i].value);
+        var endYear = parseDate(endYearInputs[i].value);
         
-        if (startYear == "Invalid Date" || endYear == "Invalid Date") {
-            alert('Please enter the dates for pair ' + (i + 1) + '.');
+        if (!startYear || !endYear) {
+            alert('Please enter valid dates for pair ' + (i + 1) + '.');
+            return;
+        }
+
+        if (isNaN(startYearInputs[i].value) || isNaN(endYearInputs[i].value)) {
+            alert('Make sure the dates are numbers for pair ' + (i + 1) + '.');
+            return;
+        }
+
+        if (startYear > endYear) {
+            alert('Start date cannot be greater than end date for pair ' + (i + 1) + '.');
+            return;
+        }
+
+        if (startYearInputs[i].value.length != 8 || endYearInputs[i].value.length != 8) {
+            alert('Please enter valid dates in YYYYMMDD for pair ' + (i + 1) + '.');
             return;
         }
 
@@ -61,3 +72,17 @@ year_form.addEventListener('submit', function (event) {
 
     document.getElementById('total-year').innerHTML = 'Total of ' + sumYears + ' years, ' + sumMonths + ' months, ' + sumDays + ' days' ;
 });
+
+function parseDate(dateString) {
+    var year = dateString.substr(0, 4);
+    var month = dateString.substr(4, 2);
+    var day = dateString.substr(6, 2);
+
+    var date = new Date(year, parseInt(month) - 1, day);
+
+    if (isNaN(date.getTime())) {
+        return null;
+    }
+
+    return date;
+}
